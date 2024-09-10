bd4aa77390d97715ca30dfdebe7678ecadad36bc
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 20:22:23 +0200

    Factorial calculation start

---
+++ Loops_and_conditionals/Factorial_calculation.cpp
@@ -0,0 +1,16 @@
+/*
+Factorial Calculation
+
+Task: Write a program that calculates the factorial of a given number (n!). 
+The user should input a number, and the program calculates the factorial using a loop.
+
+Practice: Loops (multiplying values in the factorial) and conditionals (checking valid input).
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
