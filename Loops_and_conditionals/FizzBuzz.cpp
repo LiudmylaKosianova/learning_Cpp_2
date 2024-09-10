b87ea882defb0efcfd67a2d2a94585fbaa6bd568
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 15:27:06 +0200

    FizzBuzz.cpp started

---
+++ Loops_and_conditionals/FizzBuzz.cpp
@@ -0,0 +1,21 @@
+/*
+FizzBuzz Program
+
+Task: Write a program that prints numbers from 1 to 100. 
+But for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz". 
+For numbers that are multiples of both 3 and 5, print "FizzBuzz".
+
+Practice: Loops (e.g., for, while) and conditionals (if, else if, else).
+*/
+
+#include <iostream>
+
+int main(){
+
+    for(int i = 1; i < 101; i++){
+        std::cout << i << std::endl;
+    }
+
+
+    return 0;
+}
