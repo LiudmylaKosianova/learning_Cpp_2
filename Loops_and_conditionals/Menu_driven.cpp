22a50a858b1ab79e7f0fca8f475cdbd09f2d37b7
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 20:37:50 +0200

    Menu driven started

---
+++ Loops_and_conditionals/Menu_driven.cpp
@@ -0,0 +1,20 @@
+/*
+Menu-Driven Program
+
+Task: Create a program that presents a menu to the user with options 
+(e.g., 1. Addition, 2. Subtraction, 3. Multiplication, 4. Division, 5. Exit). 
+The user selects an option and inputs two numbers, and the program performs the selected operation. 
+The program should loop until the user selects "Exit."
+
+Practice: Loops (for repeating the menu), conditionals (to check which option is selected), 
+and basic arithmetic operations.
+*/
+
+#include <iostream>
+
+int main()
+{
+    
+
+    return 0;
+}
