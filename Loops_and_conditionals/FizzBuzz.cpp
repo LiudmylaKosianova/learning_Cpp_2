605c988691fce368bfdadd6e4f39a5f63d4ca618
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 15:34:43 +0200

    Fizz added

--- Loops_and_conditionals/FizzBuzz.cpp
+++ Loops_and_conditionals/FizzBuzz.cpp
@@ -12,8 +12,16 @@ Practice: Loops (e.g., for, while) and conditionals (if, else if, else).
 
 int main(){
 
-    for(int i = 1; i < 101; i++){
-        std::cout << i << std::endl;
+    for(int i = 1; i < 101; i++)
+    {
+        if(i%3 == 0 && i%5 != 0)
+        {
+            std::cout << "Fizz\n";
+        }         
+        else
+        {        
+            std::cout << i << std::endl;
+        }
     }
 
 
