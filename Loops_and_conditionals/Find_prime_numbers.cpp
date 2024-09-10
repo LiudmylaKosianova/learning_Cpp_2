a66c62368f44b8ca73e84c59b68a246a77d0af10
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 19:54:15 +0200

    Find prime numbers finished

--- Loops_and_conditionals/Find_prime_numbers.cpp
+++ Loops_and_conditionals/Find_prime_numbers.cpp
@@ -11,13 +11,21 @@ Practice: Loops (nested loops for checking divisibility) and conditionals (for d
 
 int main()
 {
-    
-    for (int i = 3; i < 101; i++)
+    bool prime = true;
+
+    for (int i = 2; i < 101; i++)
     {
         for(int j = 2; j < i; j++)
         {
-            if(i%j == 0){break;}
+            if(i%j == 0)
+            {
+                prime = false;
+                break;
+            }
         }
+         
+        if(prime){std::cout << i << " is prime\n";}
+        prime = true;
     }
 
     return 0;
