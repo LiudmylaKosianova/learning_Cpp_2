d1fec9e444a6c587bb4baf9db002e12c6acbb247
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 19:48:33 +0200

    Find prime numbers in progress

---
+++ Loops_and_conditionals/Find_prime_numbers.cpp
@@ -0,0 +1,24 @@
+/*
+Find Prime Numbers
+
+Task: Write a program that prints all prime numbers between 1 and 100. 
+A prime number is a number greater than 1 that has no divisors other than 1 and itself.
+
+Practice: Loops (nested loops for checking divisibility) and conditionals (for determining if a number is prime).
+*/
+
+#include <iostream>
+
+int main()
+{
+    
+    for (int i = 3; i < 101; i++)
+    {
+        for(int j = 2; j < i; j++)
+        {
+            if(i%j == 0){break;}
+        }
+    }
+
+    return 0;
+}
