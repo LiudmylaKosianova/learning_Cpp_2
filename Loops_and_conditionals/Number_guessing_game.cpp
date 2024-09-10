0f8784a536ed8fdf25103a8cee0ef688517eff09
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-10 16:28:18 +0200

    Number guessing game started

---
+++ Loops_and_conditionals/Number_guessing_game.cpp
@@ -0,0 +1,17 @@
+/*
+Number Guessing Game
+
+Task: Write a number guessing game where the program randomly selects a number between 1 and 100. 
+The user has to guess the number, and the program gives hints like "too high" or "too low" after each guess. 
+The loop continues until the user guesses the correct number.
+
+Practice: Loops (for user input), conditionals (for comparison and feedback), and generating random numbers.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
