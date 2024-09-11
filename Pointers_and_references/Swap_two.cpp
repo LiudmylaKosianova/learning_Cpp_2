8fd215c8683c67ca5d32c9765c3a61df240310aa
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-11 18:55:55 +0200

    Swap two numbers addded

---
+++ Pointers_and_references/Swap_two.cpp
@@ -0,0 +1,20 @@
+/*
+Exercise 2: Swapping Two Numbers Using Pointers
+
+Write a C++ program that:
+
+Takes two integers as input.
+Uses a function to swap their values using pointers.
+
+Requirements:
+
+The function should take two integer pointers as parameters and swap the values they point to.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+    return 0;
+}
