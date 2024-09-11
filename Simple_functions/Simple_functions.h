66eae157921fcb70efe9f04c0a4ac288cdf78fa7
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-11 07:28:57 +0200

    Temperature convertion functions started

--- Simple_functions/Simple_functions.h
+++ Simple_functions/Simple_functions.h
@@ -2,3 +2,27 @@
 
 #include <iostream>
 
+/*
+Temperature Conversion Functions
+
+Task: Write two functions: one to convert temperatures from Celsius to Fahrenheit, 
+and another to convert Fahrenheit to Celsius. Your program should allow the user to input 
+a temperature and select which conversion they want to perform.
+
+Practice: Defining and calling functions, passing arguments, and returning values.
+*/
+
+int CelsToFahr(int cels)
+{
+    return 0;
+}
+
+int FahrToCels(int fahr)
+{
+    return 0;
+}
+
+void AskUser()
+{
+    
+}
