12c1c5d7913dab2477445bf687ec269d7ce5f6ce
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-12 12:45:53 +0200

    Account in progress

--- Simple_OOP/Bank_account_system/headers/Account.h
+++ Simple_OOP/Bank_account_system/headers/Account.h
@@ -2,3 +2,14 @@
 
 #include <iostream>
 
+class Account
+{
+    protected:
+    int accountNumber;
+    double balance;
+
+    public:
+    Account(int accountNumber, double balance);
+    virtual ~Account();
+
+};
--- Simple_OOP/Bank_account_system/src/Account.cpp
+++ Simple_OOP/Bank_account_system/src/Account.cpp
@@ -1,1 +1,12 @@
 #include "../headers/Account.h"
+
+Account::Account(int accountNumber, double balance) : accountNumber(accountNumber), balance(balance)
+{
+    std::cout << "Created account number: " << accountNumber <<
+            " with balance: " << balance << std::endl;
+}
+
+Account::~Account()
+{
+    std::cout << "Closed account number: " << accountNumber << std::endl;
+}
