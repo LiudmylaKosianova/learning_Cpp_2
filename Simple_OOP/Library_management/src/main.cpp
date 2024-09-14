3e45399f75a73a36dc8a9444417788ebc4fd7521
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-09-14 15:45:43 +0200

    Library management main in progress

---
+++ Simple_OOP/Library_management/src/main.cpp
@@ -0,0 +1,16 @@
+#include "../headers/Book.h"
+#include "../headers/EBook.h"
+#include "../headers/PrintedBook.h"
+
+int main()
+{
+    Book* library[4];
+    library[0] = new EBook("1984", "George Orwell", "123-456-789", "PDF");
+    library[1] = new PrintedBook("To Kill a Mockingbird", "Harper Lee", "987-654-321", 281);
+    library[2] = new EBook("The Martian", "Andy Weir", "321-654-987", "EPUB");
+    library[3] = new PrintedBook("Moby Dick", "Herman Melville", "456-789-123", 635);
+
+    
+
+    return 0;
+}
